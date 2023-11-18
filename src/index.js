5766fd28dd4f3914f319b320c3eda1fb14b0be1a
Janne57 <evgeniyahome1981@gmail.com>
2023-11-19 01:51:35 +0200

    fix persistor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import {store} from './components/redux/store.js';
-import {store, persistor} from './components/redux/store';
+// import {store, persistor} from './components/redux/store.js';
+import {store } from './components/redux/store';
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App.jsx';
